1c66dd8feb1fd4c6187304e36ebd04539bde053d
francomatteo88 <francomatteo88@gmail.com>
2017-11-18 09:22:44 +0100

    test

--- src/pages/search-modal/search-modal.ts
+++ src/pages/search-modal/search-modal.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { NavController, NavParams,ViewController } from 'ionic-angular';
 
 @Component({
@@ -9,10 +9,18 @@ export class SearchModalPage {
 
   searchkey :any;
 
+  @ViewChild('mainSearchbar') searchBar: Searchbar
+  
   constructor(public navCtrl: NavController, navParams: NavParams, public viewCtrl: ViewController) {
      this.searchkey = navParams.get('searchkey');
 
   }  
+  
+  ionViewDidEnter() {
+	setTimeout(()=>{
+      this.searchBar.setFocus();
+	}, 150);
+  }
   
   cancel() {
     this.viewCtrl.dismiss();
