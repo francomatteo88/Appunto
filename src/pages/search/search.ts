af9fffffbd5a9062904545498e17f0d34f20dd83
francomatteo88 <francomatteo88@gmail.com>
2017-12-01 20:34:08 +0100

    test

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -9,15 +9,6 @@ import { LoginPage } from '../login/login';
 import { ProfilePage } from '../profile/profile';
 import { Storage } from '@ionic/storage';
 import { Geolocation } from '@ionic-native/geolocation';
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  GoogleMapOptions,
-  CameraPosition,
-  MarkerOptions,
-  Marker
- } from '@ionic-native/google-maps';
 
 @IonicPage()
 @Component({
@@ -191,49 +182,9 @@ export class SearchPage {
 
 
   ionViewDidLoad() {
-    this.loadMap();
+ 
    }
  
-  loadMap() {
- 
-     let mapOptions: GoogleMapOptions = {
-       camera: {
-         target: {
-           lat: 43.0741904,
-           lng: -89.3809802
-         },
-         zoom: 18,
-         tilt: 30
-       }
-     };
- 
-     this.map = this.googleMaps.create('map_canvas', mapOptions);
- 
-     // Wait the MAP_READY before using any methods.
-     this.map.one(GoogleMapsEvent.MAP_READY)
-       .then(() => {
-         console.log('Map is ready!');
- 
-         // Now you can use all methods safely.
-         this.map.addMarker({
-             title: 'Ionic',
-             icon: 'blue',
-             animation: 'DROP',
-             position: {
-               lat: 43.0741904,
-               lng: -89.3809802
-             }
-           })
-           .then(marker => {
-             marker.on(GoogleMapsEvent.MARKER_CLICK)
-               .subscribe(() => {
-                 alert('clicked');
-               });
-           });
- 
-       });
-   }
-
 
 
 }
